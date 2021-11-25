fe8f0672d7b1a78cf2b17f3131686006dfff8ea6
creasy-pita <hz_ljq@live.com>
2021-11-25 08:54:38 +0800

    1 idea profile测试 2 resttemplate   HttpServerErrorException异常模拟

--- SpringBoot-RestTemplate/src/main/java/com/creasypita/resttemplate/controller/DemoController.java
+++ SpringBoot-RestTemplate/src/main/java/com/creasypita/resttemplate/controller/DemoController.java
@@ -1,12 +1,12 @@
 package com.creasypita.resttemplate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -27,4 +27,54 @@ public class DemoController {
 
         return restTemplate.exchange("http://192.168.9.83:19010/rulesengine/home/homeData?xmxxBsm=81", HttpMethod.GET, entity, String.class).getBody();
     }
+    @RequestMapping(value = "/template/a")
+    public String restTemplateWithHttpStatusCodeException(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <String> entity = new HttpEntity<String>(headers);
+
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = restTemplate.exchange("http://192.168.100.66:10000/template/b", HttpMethod.GET, entity, String.class);
+        } catch (HttpServerErrorException | HttpClientErrorException e) {
+            System.out.println("跑错了");
+            System.out.println( e.getStatusCode());
+            String.valueOf(e.getResponseBodyAsByteArray());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("跑错了");
+            e.printStackTrace();
+        }
+
+        return  responseEntity.getBody();
+    }
+
+    @RequestMapping(value = "/template/c")
+    public String restTemplate_status500_in_responseentityMap(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <String> entity = new HttpEntity<String>(headers);
+
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = restTemplate.exchange("http://192.168.100.66:10000/template/b", HttpMethod.GET, entity, String.class);
+        } catch (Exception e) {
+            System.out.println("跑错了");
+            e.printStackTrace();
+        }
+
+        return  responseEntity.getBody();
+    }
+
+    @RequestMapping(value = "/template/b")
+    public String getB() throws Exception {
+        ResponseEntity<String> responseEntity = new ResponseEntity<String>("500",HttpStatus.INTERNAL_SERVER_ERROR);
+        throw new Exception("jjjjjjj");
+    }
+
+    @RequestMapping(value = "/template/d")
+    public String getD() throws Exception {
+        return "ddddd";
+    }
+
 }
