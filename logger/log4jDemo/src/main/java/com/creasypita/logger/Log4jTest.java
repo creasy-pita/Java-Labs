d115cccc29269d01d5c48a9331cd337890aafecf
creasy-pita <hz_ljq@live.com>
2023-03-07 10:56:22 +0800

    log4j日志输出简单入门demo

---
+++ logger/log4jDemo/src/main/java/com/creasypita/logger/Log4jTest.java
@@ -0,0 +1,17 @@
+package com.creasypita.logger;
+import org.apache.log4j.Logger;
+/**
+ * Created by lujq on 3/7/2023.
+ */
+public class Log4jTest {
+    // 创建 logger 实例
+    private static final Logger logger = Logger.getLogger(Log4jTest.class);
+
+    public static void main(String[] args) {
+        logger.debug("Debug log");
+        logger.info("Info log");
+        logger.warn("Warn log");
+        logger.error("Error log");
+        logger.fatal("Fatal log");
+    }
+}
