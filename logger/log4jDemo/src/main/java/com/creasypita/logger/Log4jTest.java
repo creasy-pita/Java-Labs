983c8624826d1eed3f98f50b5ea99d8ab0ebc1a6
creasy-pita <hz_ljq@live.com>
2023-05-20 16:18:05 +0800

    log4j.rootLogger配置不同输出端使用不同的日志级别 比如 配置实现 log4j.rootLogger 同时设置 Console 输出 info 级别，RollingFile 输出 debug 级别

--- logger/log4jDemo/src/main/java/com/creasypita/logger/Log4jTest.java
+++ logger/log4jDemo/src/main/java/com/creasypita/logger/Log4jTest.java
@@ -8,10 +8,12 @@ public class Log4jTest {
     private static final Logger logger = Logger.getLogger(Log4jTest.class);
 
     public static void main(String[] args) {
-        logger.debug("Debug log");
-        logger.info("Info log");
-        logger.warn("Warn log");
-        logger.error("Error log");
-        logger.fatal("Fatal log");
+        for (int i = 0; i < 10000; i++) {
+            logger.debug("Debug log");
+            logger.info("Info log");
+            logger.warn("Warn log");
+            logger.error("Error log");
+            logger.fatal("Fatal log");
+        }
     }
 }
