1544391a587fe5d07ed6c5ab80c960de02f65c39
creasy-pita <hz_ljq@live.com>
2023-07-01 17:26:04 +0800

    自定义classloader加载不同包下的两个同名mysql驱动类

--- java-core/ClassLoader/readme.md
+++ java-core/ClassLoader/readme.md
@@ -3,3 +3,12 @@
 ### 场景 自定义classloader加载不同包下的两个同名类
 
 使用两个MyWebAppLoader实例加载不同路径下的com.creasypita.Person的类，调用doSomething方法查看效果，发现是连个隔离的Person类
+
+
+### 
+
+
+classloader invoke static method
+
+invoke class.forname use classloader
+https://www.geeksforgeeks.org/class-fornamestring-boolean-classloader-method-in-java-with-examples/
---
+++ java-core/ClassLoader/src/main/java/com/creasypita/MyDBClassLoaderTest.java
@@ -0,0 +1,50 @@
+package com.creasypita;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+/**
+ * Created by lujq on 7/1/2023.
+ */
+public class MyDBClassLoaderTest {
+    static final String USER = "root";
+    static final String PASS = "123456";
+    static final String DB_URL = "jdbc:mysql://192.168.100.66:3306/platform?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2b8&allowMultiQueries=true";
+    static final String MYSQL5X_JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String MYSQL8X_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static void main(String[] args) throws MalformedURLException, NoSuchMethodException {
+        MyWebAppLoader a = new MyWebAppLoader("E:\\work\\myproject\\java\\Java-Labs\\java-core\\ClassLoader\\src\\main\\resources\\a\\");
+        try {
+            //注册jdbc驱动
+            Class<?> driverClazz = a.loadClass(MYSQL5X_JDBC_DRIVER);
+            Class<?> drivermanagerClazz = a.loadClass("java.sql.DriverManager");
+            //获取连接
+            Method getConnection = drivermanagerClazz.getMethod("getConnection", String.class, String.class, String.class);
+            Object connObj = getConnection.invoke(null, DB_URL, USER, PASS);
+            //输出连接类的classloader
+            System.out.println(connObj.getClass().getClassLoader());
+
+        } catch (ClassNotFoundException |  IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+
+        MyWebAppLoader b = new MyWebAppLoader("E:\\work\\myproject\\java\\Java-Labs\\java-core\\ClassLoader\\src\\main\\resources\\b\\");
+        try {
+            //注册jdbc驱动
+            Class<?> driverClazz = b.loadClass(MYSQL8X_JDBC_DRIVER);
+            Class<?> drivermanagerClazz = b.loadClass("java.sql.DriverManager");
+            //获取连接
+            Method getConnection = drivermanagerClazz.getMethod("getConnection", String.class, String.class, String.class);
+            Object connObj = getConnection.invoke(null, DB_URL, USER, PASS);
+            //输出连接类的classloader
+            System.out.println(connObj.getClass().getClassLoader());
+
+        } catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
