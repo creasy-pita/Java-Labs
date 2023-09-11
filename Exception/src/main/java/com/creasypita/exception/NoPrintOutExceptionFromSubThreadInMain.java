135bee6da5e10d87e27dec86403e4320fbbc03f9
creasy-pita <hz_ljq@live.com>
2023-09-11 19:14:53 +0800

    doc:完善注释

--- Exception/src/main/java/com/creasypita/exception/NoPrintOutExceptionFromSubThreadInMain.java
+++ Exception/src/main/java/com/creasypita/exception/NoPrintOutExceptionFromSubThreadInMain.java
@@ -3,7 +3,11 @@ package com.creasypita.exception;
 /**
  * Created by lujq on 8/15/2023.
  * main方法中不直接输出子线程任务执行中抛出的异常
- * 总结：子线程任务执行中抛出的异常，会跑到父线程
+ * 总结：子线程任务执行中抛出的异常，会抛到父线程
+ *  * 内层AA抛出异常如果没有catch,会抛出到出口main方法，jvm中main方法结束时走的是异常出口，而不是正常的方法出口。
+ *  * 此时会在控制台打印异常，不会被忽略。
+ *  * 延申
+ *  * 如果是子线程中的异常，也是类似main线程的情况，会走异常出口，并打印
  */
 public class NoPrintOutExceptionFromSubThreadInMain {
     public static void main(String[] args) {
